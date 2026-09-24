9e31fb5bf8c9ca77554b9c99a9c294e31b033df0
nibraz0000-dotcom <nibraz0000@gmail.com>
2026-09-24 20:20:45 +0530

    chore: suppress vite native warning for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+process.env.VITE_CONFIG_NATIVE_IGNORE_WARNING = 'true';
 
 // https://vite.dev/config/
 export default defineConfig({
